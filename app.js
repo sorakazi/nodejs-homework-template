2eee905f4903477858cf55ad595aa4af2ca4571a
sorakazi <145249696+sorakazi@users.noreply.github.com>
2024-09-03 16:06:01 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
 
-const contactsRouter = require("./routes/api/contacts");
+import { router as contactsRouter } from "./routes/api/contactsRouter.js";
 
 const app = express();
 
@@ -18,9 +18,10 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+app.use((err, _req, res, _next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
-module.exports = app;
-console.log("first");
+// module.exports = app;
+export { app };
